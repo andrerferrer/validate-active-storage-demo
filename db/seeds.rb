cac8b0debd5856d6a180729e83e32a0e599f42b7
Andre Ferrer <andrerferrer@hotmail.com>
2020-06-04 19:03:18 -0300

    Init from this boilerplate: https://github.com/andrerferrer/restaurants-boilerplate

---
+++ db/seeds.rb
@@ -0,0 +1,16 @@
+puts "Create some Restaurants"
+
+Restaurant.create! name: "Amareleen", address: "Gloria"
+Restaurant.create! name: "Verdeen", address: "Gloria"
+
+puts "Let's drink a beer"
+puts %{
+  .~~~~.
+  i====i_
+  |cccc|_)
+  |cccc|
+  `-==-'
+
+}
+
+puts "Done!"
