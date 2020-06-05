e2112e7c3bfaa566eb7c1acd88e9f99848ea7db7
Andre Ferrer <andrerferrer@hotmail.com>
2020-06-04 21:28:04 -0300

    improve the seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
-puts "Create some Restaurants"
+puts "Clean the DB"
+Restaurant.destroy_all
 
-Restaurant.create! name: "Amareleen", address: "Gloria"
-Restaurant.create! name: "Verdeen", address: "Gloria"
+
+puts "Create some Restaurants"
+Restaurant.create! name: "Amareleen", neighborhood: "Gloria"
+Restaurant.create! name: "Verdeen", neighborhood: "Gloria"
+Restaurant.create! name: "McDonalds", neighborhood: "Gloria"
 
 puts "Let's drink a beer"
 puts %{
@@ -12,5 +16,14 @@ puts %{
   `-==-'
 
 }
+
+puts "Attach some photos"
+require "open-uri"
+
+amareleen = URI.open('https://1.kekantoimg.com/Psupn1iCO1zk7YxlybKy9v8Sa1s=/400x300/s3.amazonaws.com/kekanto_pics/pics/705/264705.jpg')
+mc_donalds = URI.open('https://www.wikirio.com.br/images/thumb/d/dc/McDonald%27s_-_Catete.jpg/300px-McDonald%27s_-_Catete.jpg')
+
+Restaurant.first.photo.attach(io: amareleen, filename: 'amareleen.jpg', content_type: 'image/jpg')
+Restaurant.last.photo.attach(io: mc_donalds, filename: 'amareleen.jpg', content_type: 'image/jpg')
 
 puts "Done!"
