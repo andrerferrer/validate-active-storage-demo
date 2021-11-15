5bcf36e18b0d84ae6d5fd112a795c4869eb3ff2d
Andre Ferrer <andrerferrer@hotmail.com>
2021-11-15 22:11:00 +0100

    add custom validations to restaurant model

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,4 +1,9 @@
 class Restaurant < ApplicationRecord
   has_many :reviews
   has_one_attached :photo
+  validate :photo_is_attached
+
+  def photo_is_attached
+    errors.add(:photo, 'must be attached') unless photo.attached?
+  end
 end
