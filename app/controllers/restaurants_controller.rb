64e0a1db0b986eea8d6a2b53675d87ff190b1d1a
Andre Ferrer <andrerferrer@hotmail.com>
2021-11-15 22:15:33 +0100

    generate restaurants#new and #create for demo purposes

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -7,4 +7,23 @@ class RestaurantsController < ApplicationController
     @restaurant = Restaurant.find(params[:id])
     @review = Review.new
   end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(strong_params)
+    if @restaurant.save
+      redirect_to @restaurant
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def strong_params
+    params.require(:restaurant).permit(:photo, :name, :address)
+  end
 end
